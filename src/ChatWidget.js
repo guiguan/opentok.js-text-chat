e4be53169e6ff320eff528df8d5646d53444b580
Salvador de la Puente <salva@unoyunodiez.com>
2015-09-23 19:39:20 +0200

    Now the widget accept links

--- src/ChatWidget.js
+++ src/ChatWidget.js
@@ -3,9 +3,12 @@
 import Chat from './Chat';
 import { ChatUI, ChatMessage } from './ChatUI';
 
+var links = /https?\:\/\/[^.]+\..+/g;
+
 function ChatWidget(options) {
   options = options || {};
   this._chatBox = new ChatUI(options);
+  this._chatBox.render = this.render.bind(this);
   this._chatBox.disableSending();
   if (options.session) {
     this.start(options);
@@ -33,6 +36,20 @@ ChatWidget.prototype = {
   onMessageReceived: function (contents, from) {
     var message = new ChatMessage(from.connectionId, from.data, contents);
     this._chatBox.addMessage(message);
+  },
+
+  render: function (raw) {
+    var output;
+
+    // Allow multiline
+    output = raw.replace(/(\r\n|\r|\n)/g, '<br/>');
+
+    // Detect links
+    output = output.replace(links, function (href) {
+      return '<a href="' + href + '" target="_blank">' + href + '</a>';
+    });
+
+    return output;
   }
 };
 
