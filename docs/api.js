5a118749695b88d69bf2603c9a24152d346bc318
Salvador de la Puente <salva@unoyunodiez.com>
2015-09-25 19:34:49 +0200

    Adding docs for current version

---
+++ docs/api.js
@@ -0,0 +1,12 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Chat",
+        "ChatMessage",
+        "ChatUI",
+        "ChatWidget"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
